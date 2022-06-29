f6b7eed6637c00d2a8d60629062c20da93c00c8b
jrestrada <jramiroem@gmail.com>
2022-06-29 07:46:41 -0700

    Created launch file succesfully

--- launch/pipecrawler_control_launch.py
+++ launch/pipecrawler_control_launch.py
@@ -5,7 +5,7 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='pipecrawler',
-            executable='server',
+            executable='pipecrawler_server.py',
             output = 'screen'
         )
     ])
