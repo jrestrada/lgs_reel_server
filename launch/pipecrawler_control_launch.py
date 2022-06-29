28ab33b666546b636caa0b94640aa36f0af54b21
jrestrada <jramiroem@gmail.com>
2022-06-29 07:55:38 -0700

    Revert "Created launch file succesfully"
    This reverts commit f6b7eed6637c00d2a8d60629062c20da93c00c8b.

--- launch/pipecrawler_control_launch.py
+++ launch/pipecrawler_control_launch.py
@@ -5,7 +5,7 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='pipecrawler',
-            executable='pipecrawler_server.py',
+            executable='server',
             output = 'screen'
         )
     ])
